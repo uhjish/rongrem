b1582573c74e6f71f2be471a2a581e7e6adbaf9c
Ajish <ajishg@gmail.com>
2014-02-05 23:43:57 -0500

    initial commit

---
+++ arp/settings.py
@@ -0,0 +1,25 @@
+# Scrapy settings for arp project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'arp'
+BOT_VERSION='1.0'
+SPIDER_MODULES = ['arp.spiders']
+NEWSPIDER_MODULE = 'arp.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'arp (+http://www.yourdomain.com)'
+
+
+MAX_REVIEWS = 100
+REVS_PER_PAGE = 20
+REVS_URL_PATT = 'http://www.agoda.com/pages/agoda/popup/popup_review.aspx?hotelID=HOTEL_ID'
+
+
+EXPORT_FORMAT = 'jsonlines'
+EXPORT_FILE = 'scraped_items.json'
+EXPORT_ENCODING = 'utf8'
